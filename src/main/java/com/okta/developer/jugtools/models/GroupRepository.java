bd27476468ab13cc3da21a74f3608f4a414fc020
sanjay26 <sanjay@okrasolar.com>
2020-01-25 19:43:46 +0530

    Initial commit

---
+++ src/main/java/com/okta/developer/jugtools/models/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.okta.developer.jugtools.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+}
